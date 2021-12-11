5d6d7e7101e424e631d36a004987768463c8171d
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-12-11 14:55:17 -0300

    feat: added answer route

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import * as usersRepository from "../repositories/usersRepository";
+
+async function auth(req: Request, res: Response, next: Function) {
+  try {
+    const authorization = req.headers["authorization"];
+    const token = authorization?.split("Bearer ")[1];
+
+    const session = await usersRepository.checkUser({ token: token });
+    if (session.length === 0) {
+      return res.sendStatus(401);
+    }
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+
+  next();
+}
+
+export { auth };
